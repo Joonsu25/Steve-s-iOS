b40b6e061842c1862fcecf612907235eeea2430b
Joonsu25 <officialpark98@gmail.com>
2024-01-18 02:13:50 +0900

    DOCS : 1/17

---
+++ MyApp/RPSgame/RPSgame/ViewController.swift
@@ -0,0 +1,65 @@
+//
+//  ViewController.swift
+//  RPSgame
+//
+//  Created by JUNSU PARK on 1/17/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    // 변수 // 속성
+    @IBOutlet weak var mainLabe: UILabel!
+    
+    @IBOutlet weak var comImageView: UIImageView!
+    @IBOutlet weak var myImageView: UIImageView!
+    
+    @IBOutlet weak var comChoiceLabel: UILabel!
+    @IBOutlet weak var myChoiceLabel: UILabel!
+    
+    var rpsIamge = ["가위": #imageLiteral(resourceName: "scissors"),"바위": #imageLiteral(resourceName: "rock"), "보": #imageLiteral(resourceName: "paper")]
+    
+    // 함수 // 메서드
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // 첫번째/두번째 이미지뷰에 준비(묵) 이미지를 띄워야 함
+        comImageView.image = #imageLiteral(resourceName: "ready")
+        myImageView.image = UIImage(named: "ready.png")
+        // 첫번째/두번째 레이블에 "준비"라고 문자열을 띄워야 함
+        
+    }
+    
+    @IBAction func rpsButtonTapped(_ sender: UIButton) {
+        // 가위/바위/보를 선택해서 그 정보를 저장해야 됨
+    }
+    
+    @IBAction func resetButtonTapped(_ sender: UIButton) {
+        // 1) 컴퓨터가 랜덤 선택한 것을 이미지뷰에 표시
+        // 2) 컴퓨터가 랜덤 선택한 것을 레이블에 표시
+        
+        // 3) 내가 선택한 것을 이미지뷰에 표시
+        // 4) 내가 선택한 것을 레이블에 표시
+        
+        // 5) 컴퓨터가 선택한 것과 내가 선택한 것을 비교해서 이겼는지/졌는지 판단 / 표시
+    }
+    
+    
+    @IBAction func selectButtonTapped(_ sender: UIButton) {
+        // 1) 컴퓨터가 다시 준비를 이미지뷰에 표시
+        // 2) 컴퓨터가 다시 준비를 레이블에 표시
+        
+        // 3) 내 선택 이미지뷰에도 준비를 이미지뷰에 표시
+        // 4) 내 선택 레이블에도 준비를레이블에 표시
+        
+        // 5) 메인 레이블 "선택하세요" 표시
+        
+        // 6) 컴퓨터가 다시 랜덤 가위/바위/보를 선택하고 저장.
+        
+    }
+    
+    
+
+
+}
+
